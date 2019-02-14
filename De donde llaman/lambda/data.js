6e6ee2c281168ab6b25de5556c37d9c77912c25e
Alfredo Reyes Montero <reyes-fred@hotmail.com>
2019-02-14 11:03:50 -0600

    Create data.js

---
+++ De donde llaman/lambda/data.js
@@ -0,0 +1,364 @@
+module.exports = {
+  "744": {
+    "Region":"GRO",
+    "TimezoneOffset":"-6",
+    "Description":"Acapulco, Guerrero"
+  },
+  "449": {
+    "Region":"AGU",
+    "TimezoneOffset":"-6",
+    "Description":"Aguascalientes, Aguascalientes"
+  },
+  "833": {
+    "Region":"TAM",
+    "TimezoneOffset":"-6",
+    "Description":"Altamira, Tamaulipas"
+  },
+  "81": {
+    "Region":"NL",
+    "TimezoneOffset":"-6",
+    "Description":"Apodaca, Nuevo León"
+  },
+  "624": {
+    "Region":"BCS",
+    "TimezoneOffset":"-7",
+    "Description":"Cabo San Lucas, Baja California Sur"
+  },
+  "981": {
+    "Region":"CAM",
+    "TimezoneOffset":"-6",
+    "Description":"Campeche, Yucatán"
+  },
+  "998": {
+    "Region":"ROO",
+    "TimezoneOffset":"-5",
+    "Description":"Cancún, Quintana Roo"
+  },
+  "461": {
+    "Region":"GUA",
+    "TimezoneOffset":"-6",
+    "Description":"Celaya, Guanajuato"
+  },
+  "983": {
+    "Region":"ROO",
+    "TimezoneOffset":"-5",
+    "Description":"Chetumal, Quintana Roo"
+  },
+  "614": {
+    "Region":"CHH",
+    "TimezoneOffset":"-7",
+    "Description":"Chihuahua, Ciudad Juaréz"
+  },
+  "747": {
+    "Region":"GRO",
+    "TimezoneOffset":"-6",
+    "Description":"Chilpancingo de los bravos, Guerrero"
+  },
+  "877": {
+    "Region":"COA",
+    "TimezoneOffset":"-6",
+    "Description":"Ciudad Acuña, Coahuila"
+  },
+  "656": {
+    "Region":"CHH",
+    "TimezoneOffset":"-7",
+    "Description":"Ciudad Juárez, Chihuahua"
+  },
+  "871": {
+    "Region":"DUR",
+    "TimezoneOffset":"-6",
+    "Description":"Ciudad Lerdo, Durango"
+  },
+  "833": {
+    "Region":"TAM",
+    "TimezoneOffset":"-6",
+    "Description":"Ciudad Madero, Tamaulipas"
+  },
+  "644": {
+    "Region":"SON",
+    "TimezoneOffset":"-7",
+    "Description":"Ciudad Obregon, Sonora"
+  },
+  "834": {
+    "Region":"TAM",
+    "TimezoneOffset":"-6",
+    "Description":"Ciudad Victoria, Tamaulipas"
+  },
+  "921": {
+    "Region":"VER",
+    "TimezoneOffset":"-6",
+    "Description":"Coatzacoalcos, Veracruz"
+  },
+  "312": {
+    "Region":"COL",
+    "TimezoneOffset":"-6",
+    "Description":"Colima, Colima"
+  },
+  "963": {
+    "Region":"CHI",
+    "TimezoneOffset":"-6",
+    "Description":"Comitán, Chiapas"
+  },
+  "271": {
+    "Region":"VER",
+    "TimezoneOffset":"-6",
+    "Description":"Córdoba, Veracruz"
+  },
+  "777": {
+    "Region":"MOR",
+    "TimezoneOffset":"-6",
+    "Description":"Cuernavaca, Morelos"
+  },
+  "667": {
+    "Region":"CUL",
+    "TimezoneOffset":"-7",
+    "Description":"Culiacán Rosales, "
+  },
+  "646": {
+    "Region":"MS",
+    "TimezoneOffset":"-8",
+    "Description":"Ensenada, Baja California"
+  },
+  "871": {
+    "Region":"DUR",
+    "TimezoneOffset":"-6",
+    "Description":"Gómez Palacio, Durango"
+  },
+  "33": {
+    "Region":"GDL",
+    "TimezoneOffset":"-6",
+    "Description":"Guadalajara, Jalisco"
+  },
+  "473": {
+    "Region":"GUA",
+    "TimezoneOffset":"-6",
+    "Description":"Guanajuato, Guanajuato"
+  },
+  "662": {
+    "Region":"SON",
+    "TimezoneOffset":"-7",
+    "Description":"Hermosillo, Sonora"
+  },
+   "715": {
+    "Region":"MIC",
+    "TimezoneOffset":"-6",
+    "Description":"Heroica Zitácuro, Michoacán"
+  },
+  "777": {
+    "Region":"CUE",
+    "TimezoneOffset":"-6",
+    "Description":"JIUTEPEC, Morelos"
+  },
+  "612": {
+    "Region":"BJS",
+    "TimezoneOffset":"-7",
+    "Description":"La Paz, Baja California Sur"
+  },
+  "477": {
+    "Region":"GUA",
+    "TimezoneOffset":"-6",
+    "Description":"León, Guanajuato"
+  },
+  "314": {
+    "Region":"COL",
+    "TimezoneOffset":"-6",
+    "Description":"Manzanillo, Colima"
+  },
+  "868": {
+    "Region":"TAM",
+    "TimezoneOffset":"-6",
+    "Description":"Matamoros, Tamaulipas"
+  },
+  "999": {
+    "Region":"YUC",
+    "TimezoneOffset":"-6",
+    "Description":"Mérida, Yucatán"
+  },
+  "686": {
+    "Region":"BC",
+    "TimezoneOffset":"-8",
+    "Description":"Mexicali, Baja California"
+  },
+  "55": {
+    "Region":"CDMX",
+    "TimezoneOffset":"-6",
+    "Description":"Mexico CDMX"
+  },
+  "81": {
+    "Region":"NL",
+    "TimezoneOffset":"-8",
+    "Description":"Monterrey, Nuevo León"
+  },
+  "443": {
+    "Region":"MIC",
+    "TimezoneOffset":"-6",
+    "Description":"Morelia, Michoacán"
+  },
+  "867": {
+    "Region":"TAM",
+    "TimezoneOffset":"-6",
+    "Description":"Nuevo Laredo, Tamaulipas"
+  },
+  "951": {
+    "Region":"OAX",
+    "TimezoneOffset":"-6",
+    "Description":"Oaxaca, Oaxaca"
+  },
+  "771": {
+    "Region":"HID",
+    "TimezoneOffset":"-6",
+    "Description":"Pachuca, Hidalgo"
+  },
+  "984": {
+    "Region":"ROO",
+    "TimezoneOffset":"-5",
+    "Description":"Playa del Carmen, Quintana ROO"
+  },
+  "222": {
+    "Region":"PUE",
+    "TimezoneOffset":"-6",
+    "Description":"Puebla, Puebla"
+  },
+  "322": {
+    "Region":"JAL",
+    "TimezoneOffset":"-6",
+    "Description":"Puerto Vallarta, Jalisco"
+  },
+  "899": {
+    "Region":"TAM",
+    "TimezoneOffset":"-6",
+    "Description":"Reynosa, Tamaulipas"
+  },
+  "464": {
+    "Region":"GUA",
+    "TimezoneOffset":"-6",
+    "Description":"Salamanca, Guanajuato"
+  },
+  "844": {
+    "Region":"COA",
+    "TimezoneOffset":"-6",
+    "Description":"Saltillo, Coahuila"
+  },
+  "967": {
+    "Region":"CHI",
+    "TimezoneOffset":"-6",
+    "Description":"San Cristobal de las Casas, Chiapas"
+  },
+  "444": {
+    "Region":"SLP",
+    "TimezoneOffset":"-6",
+    "Description":"San Luis Potosí, San Luis Potosí"
+  },
+  "81": {
+    "Region":"NL",
+    "TimezoneOffset":"-6",
+    "Description":"San Nicolás de los Garza, Nuevo León"
+  },
+  "615": {
+    "Region":"BCS",
+    "TimezoneOffset":"-7",
+    "Description":"Santa Rosalía, Baja California Sur "
+  },
+  "442": {
+    "Region":"QUE",
+    "TimezoneOffset":"-6",
+    "Description":"Santiago de Querétaro, Querétaro"
+  },
+  "833": {
+    "Region":"TAM",
+    "TimezoneOffset":"-6",
+    "Description":"Tampico, Tamaulipas"
+  },
+  "665": {
+    "Region":"BC",
+    "TimezoneOffset":"-8",
+    "Description":"Tecate , Baja California"
+  },
+  "311": {
+    "Region":"NAY",
+    "TimezoneOffset":"-7",
+    "Description":"Tepic, Nayarit"
+  },
+  "664": {
+    "Region":"BC",
+    "TimezoneOffset":"-8",
+    "Description":"Tijuana, Baja California"
+  },
+  "246": {
+    "Region":"TLA",
+    "TimezoneOffset":"-6",
+    "Description":"Tlaxcala de Xicohténcatl, Tlaxcala"
+  },
+  "722": {
+    "Region":"TOL",
+    "TimezoneOffset":"-6",
+    "Description":"Toluca de lerdo, Toluca "
+  },
+  "871": {
+    "Region":"COA",
+    "TimezoneOffset":"-6",
+    "Description":"Torreon, Coahuila"
+  },
+  "984": {
+    "Region":"ROO",
+    "TimezoneOffset":"-6",
+    "Description":"Tulum, Quintana Roo"
+  },
+  "961": {
+    "Region":"CHI",
+    "TimezoneOffset":"-6",
+    "Description":"Tuxtla Gutiérrez, Chiapas"
+  },
+   "452": {
+    "Region":"MIC",
+    "TimezoneOffset":"-6",
+    "Description":"Uruapan, Michoacán"
+  },
+  "985": {
+    "Region":"YUC",
+    "TimezoneOffset":"-6",
+    "Description":"Valladolid, Yucatán"
+  },
+  "457": {
+    "Region":"ZAC",
+    "TimezoneOffset":"-6",
+    "Description":"Valparaíso, Zacatecas"
+  },
+  "229": {
+    "Region":"Ver",
+    "TimezoneOffset":"-6",
+    "Description":"Veracruz, Veracruz"
+  },
+  "618": {
+    "Region":"DUR",
+    "TimezoneOffset":"-6",
+    "Description":"Victoria de Durango, Durango"
+  },
+  "993": {
+    "Region":"TAB",
+    "TimezoneOffset":"-6",
+    "Description":"Villahermosa, Tabasco"
+  },
+  "228": {
+    "Region":"VER",
+    "TimezoneOffset":"-6",
+    "Description":"Xalapa-Enrique, Veracruz"
+  },
+  "492": {
+    "Region":"ZAC",
+    "TimezoneOffset":"-6",
+    "Description":"Zacatecas, Zacatecas"
+  },
+  "351": {
+    "Region":"MIC",
+    "TimezoneOffset":"-6",
+    "Description":"Zamora, Michoacan"
+  },
+  "33": {
+    "Region":"JAL",
+    "TimezoneOffset":"-6",
+    "Description":"Zapopan, Jalisco"
+  }
+  
+  
+};
