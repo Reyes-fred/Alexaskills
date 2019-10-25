1778526d5a36362711933a1fb494813f36e4c153
Alfredo Reyes Montero <reyes-fred@hotmail.com>
2019-10-24 23:23:16 -0500

    Update main.js

--- HackBerry/lambda/main.js
+++ HackBerry/lambda/main.js
@@ -1,4 +1,3 @@
-
 // This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
 // Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
 // session persistence, api calls, and more.
@@ -13,12 +12,20 @@ var tipoList = [
 ];
 
 var tipospokemon = [
-{value : "pikachu",tags : ["Electrico"]},
-{value : "squirtle",tags : ["Agua"]},
-{value : "magikarp",tags : ["Agua"]},
-{value : "charmander",tags : ["Fuego"]},
+{value : "pikachu",tags : ["electrico"]},
+{value : "squirtle",tags : ["agua"]},
+{value : "magikarp",tags : ["agua"]},
+{value : "charmander",tags : ["fuego"]},
+{value : "Vulpix",tags : ["fuego"]},
+{value : "Cyndaquil",tags : ["fuego"]},
 ]
 
+const pokemondescripcion = {
+      charmander: {name: 'charmander', descripcion: 'Charmander es un pequeño lagarto bípedo. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola con la punta envuelta en llamas. Charmander, como sus evoluciones Charmeleon y Charizard, tiene una pequeña llama en la punta de su cola'},
+      pikachu: {name: 'pikachu', descripcion: 'Pikachu almacena una gran cantidad de electricidad en sus mejillas. Estas parecen cargarse eléctricamente durante la noche mientras duerme. Las mejillas de Pikachu también pueden ser recargadas mediante una descarga eléctrica'},
+      bulbasaur: {name: 'bulbasaur', descripcion: 'Bulbasaur es un Pokémon cuadrúpedo de color verde y manchas más oscuras de formas geométricas. Su cabeza representa cerca de un tercio de su cuerpo. En su frente se ubican tres manchas que pueden cambiar dependiendo del ejemplar. Tiene orejas pequeñas y puntiagudas. '}
+    };
+    
 function getElementsByTags(tags, arr){
   var i, j, contains;
   var ret = [];
@@ -40,11 +47,7 @@ function getElementsByTags(tags, arr){
   return ret;
 }
 
-const pokemondescripcion = {
-      Charmander: {name: 'Charmander', descripcion: 'Charmander es un pequeño lagarto bípedo. Sus características de fuego son resaltadas por su color de piel anaranjado y su cola con la punta envuelta en llamas. Charmander, como sus evoluciones Charmeleon y Charizard, tiene una pequeña llama en la punta de su cola'},
-      pikachu: {name: 'pikachu', descripcion: 'Pikachu almacena una gran cantidad de electricidad en sus mejillas. Estas parecen cargarse eléctricamente durante la noche mientras duerme. Las mejillas de Pikachu también pueden ser recargadas mediante una descarga eléctrica'},
-      bulbasaur: {name: 'bulbasaur', descripcion: 'Bulbasaur es un Pokémon cuadrúpedo de color verde y manchas más oscuras de formas geométricas. Su cabeza representa cerca de un tercio de su cuerpo. En su frente se ubican tres manchas que pueden cambiar dependiendo del ejemplar. Tiene orejas pequeñas y puntiagudas. '}
-    };
+
 
 const LaunchRequestHandler = {
     canHandle(handlerInput) {
@@ -78,10 +81,10 @@ const pokemonespecificoIntentHandler = {
     },
     handle(handlerInput) {
         let speakOutput=""
-        let pokemon = handlerInput.requestEnvelope.request.intent.slots.pokemon.value;
-        if(tipospokemon.hasOwnProperty(pokemon)){
-             let requestedpokemondescripcion = tipospokemon[pokemon].descripcion;
-              speakOutput = `${pokemon.charAt(0).toUpperCase()}${pokemon.slice(1)} dice ${requestedpokemondescripcion}!`;
+        let pokemon = handlerInput.requestEnvelope.request.intent.slots.pokemon.value.toLowerCase();
+        if(pokemondescripcion.hasOwnProperty(pokemon)){
+             let requestedpokemondescripcion = pokemondescripcion[pokemon].descripcion;
+             speakOutput = `${requestedpokemondescripcion}!`;
         }
         else{
              speakOutput = 'Perdona no tengo conocimiento de ese pokemon';}
@@ -98,18 +101,19 @@ const tipopokemonIntentHandler = {
     },
     handle(handlerInput) {
         let speakOutput=""
-        let pokemon = handlerInput.requestEnvelope.request.intent.slots.pokemon.value.toLowerCase();
-        let pokemonescat = getElementsByTags(pokemon, tipospokemon);
+        let tipo = handlerInput.requestEnvelope.request.intent.slots.tipo.value.toLowerCase();
+        let pokemonescat = getElementsByTags([tipo], tipospokemon);
         let outpokemon="";
     if(pokemonescat < 1){
       speakOutput = "No tengo pokemones de ese tipo, porfavor vuelve a intentar";
-    }else{
-		for(let x=0;x<pokemonescat.length-1;x++){
+    }
+    else{
+		for(let x=0;x<pokemonescat.length;x++){
 			outpokemon+=pokemonescat[x].value+" , ";
 		}
-	  speakOutput = 'Los pokemones de tipo '+pokemon+' son: '+outpokemon;
+	  speakOutput = 'Los pokemones de tipo '+tipo+' son: '+outpokemon;
      
-}
+    }
     
         return handlerInput.responseBuilder
             .speak(speakOutput)
@@ -182,7 +186,7 @@ const ErrorHandler = {
     },
     handle(handlerInput, error) {
         console.log(`~~~~ Error handled: ${error.stack}`);
-        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
+        const speakOutput = `Perdona hubo un error. Porfavor vuelve a intentar.`;
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
